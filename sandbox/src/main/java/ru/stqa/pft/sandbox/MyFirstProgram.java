d41707f7402984cf210355476cbc154b739ad5f4
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-11-26 22:53:52 -0800

    Вычисление расстояния между точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -7,21 +7,27 @@ public class MyFirstProgram {
     hello("user");
     hello("Iriska");
 
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-    double a = 4;
-    double b = 6;
-    System.out.println("Площадь прямоугольника со сторонами " + a + " и "+ b + " = " + area(a,b));
+    Rectangle t = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + t.a + " и "+ t.b + " = " + area(t));
+
+    Point a = new Point (1, 2);
+    Point b = new Point (3, 4);
+    System.out.println("Расстояние между точками = " + distance(a, b));
   }
 
 	public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
   }
-  public static double area(double l) {
-    return l * l;
+  public static double area(Square s) {
+    return s.l * s.l;
   }
-  public static double area(double a, double b) {
-    return a * b;
+  public static double area(Rectangle t) {
+    return t.a * t.b;
+  }
+  public static double distance(Point a, Point b) {
+    return Math.sqrt((b.x - a.x)*(b.x - a.x) + (b.y - a.y)*(b.y - a.y));
   }
 } 
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Sviatoslav on 11/26/2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+
+  this.x = x;
+  this.y = y;
+  }
+
+}
