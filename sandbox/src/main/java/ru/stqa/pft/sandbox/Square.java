b3e449303958039e9a4c9b2f6b68ed501c098130
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-11-26 22:53:26 -0800

    Созданы классы Square и Rectangle для вычисления площади

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Square.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by Sviatoslav on 11/26/2016.
+ */
+public class Square {
+  public double l;
+
+  public Square (double l) {
+    this.l = l;
+  }
+}
