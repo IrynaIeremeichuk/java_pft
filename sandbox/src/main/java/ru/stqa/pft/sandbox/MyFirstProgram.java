2bfca47275cc47f72908783406233d91c0a28ce6
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-08 17:49:30 -0800

    Функция public static double distance заменена на метод a.distance(b)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,9 +15,9 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p1 = new Point(5, 9);
-    Point p2 = new Point(3, 5);
-    System.out.println("Расстояние между точками с координатами " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " = " + distance (p1, p2));
+    Point a = new Point(5, 9);
+    Point b = new Point(3, 5);
+    System.out.println("Расстояние между точками с координатами " + a.x + "," + a.y + " и " + b.x + "," + b.y + " = " + a.distance (b));
 
     }
 
@@ -26,8 +26,4 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-
-  public static double distance(Point p1, Point p2) {
-    return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
-  }
 }
