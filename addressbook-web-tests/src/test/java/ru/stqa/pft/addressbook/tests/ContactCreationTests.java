ee3e529b826a0bc92250a4057aab4b2d29a14427
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-11 00:04:21 -0800

    Класс по созданию контакта перенесен в пакет tests, добавленны ссылки на вспомогательные методы

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+
+public class ContactCreationTests extends TestBase {
+
+  @Test
+  public void testContactCreation() {
+    app.getContactHelper().initContactCreation();
+    app.getContactHelper().fillContactForm(new ContactData("Ira", "Me", "Me_Ira", "QA", "uTest", "online", "8188267879", "8188267879", "ira@gmail.com", "1985", "Fremont, CA"));
+    app.getContactHelper().submitContactCreation();
+    app.getNavigationHelper().returnToHomePage();
+  }
+
+}
