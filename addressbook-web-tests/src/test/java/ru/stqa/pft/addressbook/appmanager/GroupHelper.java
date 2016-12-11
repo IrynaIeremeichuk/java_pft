fb1d1cbb454c635094bc25face9f79c1f500ca7c
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-10 19:57:03 -0800

    Отредактированна ссылка на драйвер FirefoxDriver wd

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -8,7 +8,11 @@ import ru.stqa.pft.addressbook.model.GroupData;
  * Created by Sviatoslav on 07.12.2016.
  */
 public class GroupHelper {
-  FirefoxDriver wd;
+  private FirefoxDriver wd;
+
+  public GroupHelper(FirefoxDriver wd) {
+    this.wd=wd;
+  }
 
   public void returnToGroupPage() {
     wd.findElement(By.linkText("group page")).click();
