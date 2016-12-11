6c9895a0d22ce8adcd6a925c4995fdef0778f574
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-11 00:08:40 -0800

    В класс NavigationHelper добавлена навигация, связанная с созданием контакта

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Sviatoslav on 10.12.2016.
+ */
+public class NavigationHelper extends HelperBase {
+
+  public NavigationHelper(FirefoxDriver wd) {
+    super(wd);
+  }
+  public void gotoGroupPage() {
+    click(By.linkText("groups"));
+  }
+  public void gotoHomePage() {
+    click(By.linkText("home"));
+  }
+  public void returnToHomePage() {
+    click(By.linkText("home"));
+  }
+}
