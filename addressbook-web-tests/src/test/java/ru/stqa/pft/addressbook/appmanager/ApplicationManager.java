dc3b5a0d045f6fadd3e0f00ae0eec5ec7221ac6b
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-11 00:24:36 -0800

    Добавлено делегирование ContactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -1,7 +1,5 @@
 package ru.stqa.pft.addressbook.appmanager;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -12,17 +10,12 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
   FirefoxDriver wd;
 
+  private ContactHelper contactHelper;
   private SessionHelper sessionHelper;
-  private NavigashionHelper navigashionHelper;
+  private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
 
-  public static boolean isAlertPresent(FirefoxDriver wd) {
-    try {
-      wd.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
+  public ApplicationManager() {
   }
 
   public void init() {
@@ -30,10 +23,12 @@ public class ApplicationManager {
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/");
     groupHelper = new GroupHelper(wd);
-    navigashionHelper = new NavigashionHelper(wd);
+    contactHelper = new ContactHelper(wd);
+    navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
     sessionHelper.login("admin", "secret");
   }
+
   public void stop() {
     wd.quit();
   }
@@ -42,7 +37,14 @@ public class ApplicationManager {
     return groupHelper;
   }
 
-  public NavigashionHelper getNavigashionHelper() {
-    return navigashionHelper;
+  public NavigationHelper getNavigationHelper() {
+    return navigationHelper;
   }
+
+  public ContactHelper getContactHelper() {
+    return contactHelper;
+  }
+
+
 }
+
