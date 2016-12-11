02d9e53fdebc0aca7629cec7bfbb44b9fdb91ce2
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-11 00:21:27 -0800

    Исправлено имя NavigationHelper

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,22 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sviatoslav on 07.12.2016.
  */
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  public final ApplicationManager app = new ApplicationManager();
+
 
   @BeforeMethod
   public void setUp() throws Exception {
