2e883ee1673d3d1b9a7819c06f56480253c748e5
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-15 15:12:27 -0800

    Исполняемые файлы для IE и Chrome

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,21 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.NoAlertPresentException;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Sviatoslav on 07.12.2016.
  */
 public class TestBase {
 
-  public final ApplicationManager app = new ApplicationManager();
+  public final ApplicationManager app = new ApplicationManager(BrowserType.IE);
 
 
   @BeforeMethod
