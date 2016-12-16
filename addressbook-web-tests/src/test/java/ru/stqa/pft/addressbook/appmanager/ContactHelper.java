c40ab913cb3e522af8234b971271dc622e6778e3
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-15 19:58:14 -0800

    Добавленна проверка выполняемого действия и наличия поля для выбора группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContactHelper.java
@@ -1,8 +1,11 @@
 package ru.stqa.pft.addressbook.appmanager;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 /**
@@ -18,7 +21,7 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//div[@id='content']/form/input[21]"));
   }
 
-  public void fillContactForm(ContactData contactData) {
+  public void fillContactForm(ContactData contactData, boolean creation) {
     type(By.name("firstname"), contactData.getFirstname());
     type(By.name("lastname"), contactData.getLastname());
     type(By.name("nickname"), contactData.getNickname());
@@ -32,7 +35,16 @@ public class ContactHelper extends HelperBase {
     click(By.xpath("//div[@id='content']/form/select[2]//option[2]"));
     type(By.name("byear"), contactData.getByear());
     type(By.name("address2"), contactData.getAddress2());
+
+    if(creation) {
+      new Select(wd.findElement(By.name("new_group"))).selectByVisibleText(contactData.getGroup());
+    } else {
+      Assert.assertFalse(isElementPresent(By.name("new_group")));
+    }
+
   }
+
+
   public void initContactCreation() {
     click(By.linkText("add new"));
   }
