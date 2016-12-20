9e16c600a6bbb405e85f5b999fbc49fc415a5c5d
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-20 00:25:19 -0800

    Работа с коллекциями

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Sviatoslav on 18.12.2016.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python");
+    for (String l: languages) {
+      System.out.println("Я учу " + l);
+    }
+  }
+}
