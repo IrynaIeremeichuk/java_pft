75ccb2e54158f7dcfe79b0cac5e8a715a2fc137d
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-22 21:02:02 -0800

    Цикл по поиску максимального значения id оптимизирован через Лямбда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -25,7 +26,8 @@ public class GroupCreationTests extends TestBase {
         max = g.getId();
       }
     }
-    group.setId(max);
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
   }
