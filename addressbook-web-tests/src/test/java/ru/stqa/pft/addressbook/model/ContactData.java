a33ce737471c408b1e6851a78165ae9f8b7b768a
Iryna Ieremeichuk <ieremeichukiryna@gmail.com>
2016-12-25 19:25:40 -0800

    Добавлены: переменная id, toString и Equals.
    + второй конструктор ContactData с учетом id

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,6 +1,11 @@
 package ru.stqa.pft.addressbook.model;
 
 public class ContactData {
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  private int id;
   private final String firstname;
   private final String lastname;
   private String group;
@@ -14,7 +19,8 @@ public class ContactData {
   private final String byear;
   private final String address2;
 
-  public ContactData(String firstname, String lastname, String group, String nickname, String title, String company, String address, String mobilephone, String workphone, String email, String byear, String address2) {
+  public ContactData(int id, String firstname, String lastname, String group, String nickname, String title, String company, String address, String mobilephone, String workphone, String email, String byear, String address2) {
+    this.id = id;
     this.firstname = firstname;
     this.lastname = lastname;
     this.group = group;
@@ -28,6 +34,50 @@ public class ContactData {
     this.byear = byear;
     this.address2 = address2;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+
+    ContactData that = (ContactData) o;
+
+    if (firstname != null ? !firstname.equals(that.firstname) : that.firstname != null) return false;
+    return lastname != null ? lastname.equals(that.lastname) : that.lastname == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = firstname != null ? firstname.hashCode() : 0;
+    result = 31 * result + (lastname != null ? lastname.hashCode() : 0);
+    return result;
+  }
+
+  @Override
+  public String toString() {
+    return "ContactData{" +
+            "firstname='" + firstname + '\'' +
+            ", lastname='" + lastname + '\'' +
+            '}';
+  }
+
+  public ContactData(String firstname, String lastname, String group, String nickname, String title, String company, String address, String mobilephone, String workphone, String email, String byear, String address2) {
+    this.id = Integer.MAX_VALUE;
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.group = group;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.address = address;
+    this.mobilephone = mobilephone;
+    this.workphone = workphone;
+    this.email = email;
+    this.byear = byear;
+    this.address2 = address2;
+  }
+  public int getId() {  return id; }
 
   public String getFirstname() {
     return firstname;
@@ -76,4 +126,6 @@ public class ContactData {
   public String getGroup() {
     return group;
   }
+
+
 }
